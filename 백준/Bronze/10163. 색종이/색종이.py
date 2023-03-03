c8cb64ce9be718b3eb9210725a543d54fd8a544c
se0987 <122589779+se0987@users.noreply.github.com>
2023-03-03 13:58:18 +0900

    [Bronze I] Title: 색종이, Time: 532 ms, Memory: 123152 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,24 +1,22 @@
-arr = [[0] * 1001 for _ in range(1001)]  # 빈 101 * 101 리스트
-# for i in arr:
-#     print(i)
+paper = [[0 for _ in range(1002)] for _ in range(1002)]
+N = int(input())
+for n in range(1, N+1):
+    y, x, h, w = map(int, input().split())
+    for i in range(y, y + h):
+        for j in range(x, x + w):
+            paper[i][j] = n
+# print(*paper, sep='\n')
 
-T = int(input())
-for ct in range(1, T + 1):  # 입력받은 색종이 수 만큼 반복
-    x, y, w, h = map(int, input().split())
-    for a in range(x, (x + w )):  # x1 ~ x2
-        for b in range(y, (y + h )):
-            arr[b][a] = ct
-# print(arr)
-#
-# for i in arr:
-#     print(i)
+ans = {}
+for i in range(1, N+1):
+    ans[i] = 0
 
-cnt = [0] * T  # 색종이 수 만큼 카운트 인덱스 만들기
-for n in range(T):  # 색종위 수 만큼 반복
-    for i in range(1001):
-        for j in arr[i]:  # j가 arr[i]에 있는 항목일 때, (0~T)
-            if n + 1 == j:
-                cnt[n] += 1
+for i in range(1002):
+    for j in range(1002):
+        a = paper[i][j]
+        if a in ans:
+            ans[a] += 1
 
-for i in cnt:
-    print(i)
+
+for i in range(1, N+1):
+    print(ans[i])
