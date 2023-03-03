c2a93c5ac1239a75ffbac82daaa5d87bd25000e4
se0987 <122589779+se0987@users.noreply.github.com>
2023-03-03 11:36:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,17 +1,12 @@
-N = 9  # 난쟁이들의 수
-height = []  # 난쟁이들의 키
-Minions = []  # 찾는 난쟁이들
+from itertools import combinations
 
-for i in range(N):
-    h = int(input())
-    height.insert(i, h)  # height = [20, 7, 23, 19, 10, 15, 25, 8, 13]
+minions = [int(input()) for _ in range(9)]
+# print(minions)
 
-from itertools import combinations  # 조합 함수 불러오기
-
-for i in combinations(height, 7):  # 9개 중 7개를 뽑는다. 중복X, 순서X
-    if sum(i) == 100:  # 7개의 합이 100이라면
-        Minions = i  # 해당 7개는 미니언즈
-
-minions = sorted(Minions)  # 오름차순 정렬
-# 줄에 하나씩 출력
-print(*minions, sep='\n')
+lst = set(combinations(minions, 7))
+# print(lst)
+for i in lst:
+    if sum(i) == 100:
+        a = sorted(i)
+        print(*a, sep='\n')
+        break
