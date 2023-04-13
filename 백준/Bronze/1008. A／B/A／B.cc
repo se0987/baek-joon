34f66d2088df5866c9ad4e66bade9c35675e10db
se0987 <122589779+se0987@users.noreply.github.com>
2023-04-13 14:57:57 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	double a1 = static_cast<double>(a);
+	double b1 = static_cast<double>(b);
+
+	cout << fixed;
+	cout.precision(9);
+
+	cout << a1 / b1;
+}
